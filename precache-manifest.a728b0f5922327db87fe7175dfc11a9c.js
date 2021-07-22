d61c3500e179240454c1b40aed6179906fd5d74f
edspired <recruitment@edspired.com>
2021-07-22 21:47:12 +0000

    Updates

---
+++ precache-manifest.a728b0f5922327db87fe7175dfc11a9c.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e45049a3084b448457f9acc362429867",
+    "url": "/index.html"
+  },
+  {
+    "revision": "481b34592c44bad8bad7",
+    "url": "/static/css/main.9b3ac610.chunk.css"
+  },
+  {
+    "revision": "376c82efe4a971b6a222",
+    "url": "/static/js/2.63abd23a.chunk.js"
+  },
+  {
+    "revision": "117d65892edc8adcd29d4a0d57b5a80a",
+    "url": "/static/js/2.63abd23a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "481b34592c44bad8bad7",
+    "url": "/static/js/main.51a91485.chunk.js"
+  },
+  {
+    "revision": "10ca3dca31a2209d9acd",
+    "url": "/static/js/runtime-main.b9b7e1e9.js"
+  },
+  {
+    "revision": "3ce357e71a836a86691b904396a8955c",
+    "url": "/static/media/cta-illustration.3ce357e7.svg"
+  },
+  {
+    "revision": "3c569239f88160018f79029ba5dc227d",
+    "url": "/static/media/features-split-image-02.3c569239.png"
+  },
+  {
+    "revision": "ea6949039d6554ebf2cf109f333f5811",
+    "url": "/static/media/illustration-section-01.ea694903.svg"
+  },
+  {
+    "revision": "63d0555d90c053eea1a1a7469d004661",
+    "url": "/static/media/illustration-section-02.63d0555d.svg"
+  },
+  {
+    "revision": "2810a88b1d6365b363caeabe678f1629",
+    "url": "/static/media/logo.2810a88b.svg"
+  }
+]);
